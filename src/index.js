7d48a66b86bd79e63e04656531d6d0b56c8e0fd9
Foziljon <123368997+az1mov-f@users.noreply.github.com>
2025-08-09 23:38:15 +0500

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import "./assets/css/style.css";
-import root from "./root.vue";
-import store from "./store/index.js";
-import router from "./router/index.js";
-
-createApp(root).use(router).use(store).mount("#root");
--- src/views/ErrorPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1 class="text-red-500 text-[30px]">404 Not found</h1>
-</template>
-
-<script>
-export default {
-  name: "ErrorPage",
-};
-</script>
